ac39916d121430b0822699e77ac5f74cbd5b78d1
Sameer <Sameer@Sameers-MacBook-Pro-4.local>
2010-11-01 12:02:08 -0400

    temp portfolio setup

---
+++ temp-portfolio.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+  
+  <meta charset="utf-8">
+  <!--[if IE]><![endif]-->
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
+  <link rel="shortcut icon" href="/favicon.ico">
+  <!--  <link rel="apple-touch-icon" href="/apple-touch-icon.png">-->
+  <link rel="stylesheet" href="css/reset.css?v=1">
+  <link rel="stylesheet" media="handheld" href="css/handheld.css?v=1">
+
+  <link href="css/temp-portfolio.css" rel="stylesheet" type="text/css" />
+
+</head>
+  <!--[if lt IE 7 ]> <body class="ie6"> <![endif]-->
+  <!--[if IE 7 ]>    <body class="ie7"> <![endif]-->
+  <!--[if IE 8 ]>    <body class="ie8"> <![endif]-->
+  <!--[if IE 9 ]>    <body class="ie9"> <![endif]-->
+  <!--[if (gt IE 9)|!(IE)]><!--> <body> <!--<![endif]-->
+
+  <div id="container">
+    <header>
+    </header>
+    
+    <div id="main">
+      <a href="http://twitter.com/sameer0583" class="social tw" target="_blank" ></a>
+      <a href="http://facebook.com/sameert" class="social fb" target="_blank" ></a>
+      <a href="http://www.linkedin.com/in/sameer0583" class="social in" target="_blank" ></a>
+      <a href="mailto:sameer@sameert.com" class="social mail" ></a>
+    </div>
+
+    <footer>
+    </footer>
+  </div> <!-- end of #container -->
+
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+  <script>!window.jQuery && document.write('<script src="js/jquery-1.4.2.min.js"><\/script>')</script>
+  <script src="js/plugins.js?v=1"></script>
+  <script src="js/script.js?v=1"></script>
+
+  <!--[if lt IE 7]>
+  <script src="/js/dd_belatedpng.js?v=1"></script>
+  <![endif]-->
+
+  <!-- <script src="/js/profiling/yahoo-profiling.min.js?v=1"></script>
+  <script src="/js/profiling/config.js?v=1"></script> -->
+
+
+</body>
+</html>
