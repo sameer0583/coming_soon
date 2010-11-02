9eadb7531c3a4de34d1d0630e224539481aa16a1
Sameer <Sameer@Sameers-MacBook-Pro-4.local>
2010-11-02 14:44:24 -0400

    temp portfolio - adding clients

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
-  // do something
+  $('#main .projects').slideUp(0, function() {
+    $(this).css('opacity', '1');
+  });
+  
+  $('.show').click(function(event){
+    objClass = $(this).attr('class').split(" ");
+    visibility = objClass[2];
+    objClass = objClass[1];
+    event.preventDefault();
+    if( visibility != 'visible' ) {
+      $(this).addClass('visible');
+      $('.show.'+objClass).html('Hide Projects <span>▲</span>');
+      $('.projects.'+objClass).slideDown(1500, function() {
+        // do nothing.
+      });
+    } else {
+      $(this).removeClass('visible');
+      $('.show.'+objClass).html('Show Projects <span>▼</span>');
+      $('.projects.'+objClass).slideUp(500, function() {
+        // do nothing.
+      });
+    }
+  });
+    
 });
