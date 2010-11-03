9165438a64e1906a5dec2d0a073086d1d98c5a16
Sameer <Sameer@Sameers-MacBook-Pro-4.local>
2010-11-03 13:18:31 -0400

    temp portfolio - adding clients

---
+++ clients.php
@@ -0,0 +1,349 @@
+<?php
+  $jets = array(
+    'name' => 'New York Jets',
+    'subtitle' => 'Social Media Marketing Campaign',
+    'desc' => 'When VaynerMedia was working with the Jets, I did a wide array of social media projects ranging from giveaway micro-sites to designs for in-stadium signage.  They had a strict style guide that I had to adhere to and in working with them, there were many iterative processes along the way.',
+    'projects' => array(
+      array(
+        'name' => 'In-Stadium Signage',
+        'image' => '/images/portfolio/jets-1.jpg',
+        'desc' => 'When the Jets moved to the New Meadowlands, they wanted to add social media to their in-stadium signage.  With various sized pylons and ad spaces around the stadium, it was my job to design the social media ads for Facebook and Twitter.',
+        'skills' => array('photoshop','illustrator'),
+        'link' => array(
+          'url' => '/preview.php?image=/images/preview/jets-1.jpg',
+          'text' => 'sample image'
+        )
+      ),
+      array(
+        'name' => 'Jets Connect',
+        'image' => '/images/portfolio/jets-2.jpg',
+        'desc' => 'Conceptually a really cool project where the Jets gave away various items weekly.  In order to enter you would have to fill out the form and follow the Jets and various Jets Players.  I was in charge of the overall and weekly design elements.',
+        'skills' => array('photoshop'),
+        'link' => array(
+          'url' => '/preview.php?image=/images/preview/jets-2.jpg',
+          'text' => 'sample image'
+        )
+      ),
+      array(
+        'name' => 'MotoPic',
+        'image' => '/images/portfolio/jets-3.jpg',
+        'desc' => 'A highly social promotion sponsored by Motorola which involved Facebook users uploading photos to the Jets\' Facebook album through a microsite to win tickets. Apart from the design, I did a majority of the markup and pair programmed with the developer on the back-end development.',
+        'skills' => array('photoshop', 'illustrator', 'php', 'facebook graph API', 'fbml', 'html', 'css'),
+        'link' => array(
+          'url' => 'http://www.facebook.com/Jets?v=app_152295358120613',
+          'text' => 'facebook tab'
+        )
+      ),
+      array(
+        'name' => 'Hard Knocks',
+        'image' => '/images/portfolio/jets-4.jpg',
+        'desc' => 'A Facebook Promotion based on the HBO show Hard Knocks featuring the Jets.  After watching the show, users would answer a question to win prizes.  I did the majority of the coding and all of the overall / weekly design for this project.',
+        'skills' => array('photoshop', 'illustrator', 'php', 'fbml', 'html', 'css'),
+        'link' => array(
+          'url' => '/preview.php?image=/images/preview/jets-4.jpg',
+          'text' => 'sample image'
+        )
+      )
+    )
+  );
+  
+  $nets = array(
+    'name' => 'New Jersey Nets',
+    'subtitle' => 'Social Media Marketing Campaign',
+    'desc' => 'When VaynerMedia was working with the Jets, I did a wide array of social media projects ranging from giveaway microsites to designs for in-stadium signage.  They had a strict style guide that I had to adhere to and in working with them, there were many iterative processes along the way.',
+    'projects' => array(
+      array(
+        'name' => 'In-Stadium Signage',
+        'image' => '/images/portfolio/jets-1.jpg',
+        'desc' => 'When the Jets moved to the New Meadowlands, they wanted to add social media to their in-stadium signage.  With various sized pylons and ad spaces around the stadium, it was my job to design the social media ads for Facebook and Twitter.',
+        'skills' => array('photoshop','illustrator'),
+        'link' => array(
+          'url' => '/preview.php?image=/images/preview/jets-1.jpg',
+          'text' => 'sample image'
+        )
+      ),
+      array(
+        'name' => 'Jets Connect',
+        'image' => '/images/portfolio/jets-2.jpg',
+        'desc' => 'Conceptually a really cool project where the Jets gave away various items weekly.  In order to enter you would have to fill out the form and follow the Jets and various Jets Players.  I was in charge of the overall and weekly design elements.',
+        'skills' => array('photoshop'),
+        'link' => array(
+          'url' => '/preview.php?image=/images/preview/jets-2.jpg',
+          'text' => 'sample image'
+        )
+      ),
+      array(
+        'name' => 'MotoPic',
+        'image' => '/images/portfolio/jets-3.jpg',
+        'desc' => 'A highly social promotion sponsored by Motorola which involved Facebook users uploading photos to the Jets\' Facebook album through a microsite to win tickets. Apart from the design, I did a majority of the markup and pair programmed with the developer on the back-end development.',
+        'skills' => array('photoshop', 'illustrator', 'php', 'fbml', 'facebook graph', 'html', 'css'),
+        'link' => array(
+          'url' => 'http://www.facebook.com/Jets?v=app_152295358120613',
+          'text' => 'facebook tab'
+        )
+      ),
+      array(
+        'name' => 'Hard Knocks',
+        'image' => '/images/portfolio/jets-4.jpg',
+        'desc' => 'A Facebook Promotion based on the HBO show Hard Knocks featuring the Jets.  After watching the show, users would answer a question to win prizes.  I did the majority of the coding and all of the overall / weekly design for this project.',
+        'skills' => array('photoshop', 'illustrator', 'php', 'fbml', 'html', 'css'),
+        'link' => array(
+          'url' => '/preview.php?image=/images/preview/jets-4.jpg',
+          'text' => 'sample image'
+        )
+      )
+    )
+  );
+  
+  $bjtv = array(
+    'name' => 'BroadwayJoe.tv',
+    'subtitle' => 'Joe Namath\'s Personal Website',
+    'desc' => 'As a huge football fan, it was a great experience to design and code this project.  Joe wanted to create a website where he essentially housed his video blog and at the same time act as his personal social media hub.  This project required a complete product from logo creation to social media branding.',
+    'projects' => array(
+      array(
+        'name' => 'Logo / Branding',
+        'image' => '/images/portfolio/bj-1.jpg',
+        'desc' => 'Taking everything that the name Joe Namath stood for and portraying it in a logo was not an easy task.  Not to mention, there had to be careful attention paid to his fan-base of both Alabama Crimson Tide and New York Jets fans.',
+        'skills' => array('photoshop','illustrator'),
+        'link' => array(
+          'url' => '/preview.php?image=/images/preview/bj-1.jpg',
+          'text' => 'sample image'
+        )
+      ),
+      array(
+        'name' => 'Website',
+        'image' => '/images/portfolio/bj-2.jpg',
+        'desc' => 'For Joe Namath I wanted to code and design a website that completely and accurately portrayed his story.  Some of the cooler features are the multiple themes (Alabama Crimson Tide and New York Jets) that change when you click on the respective jerseys and the CafePress linked store with products that I designed for him.',
+        'skills' => array('photoshop', 'wordpress', 'cafepress', 'php', 'html', 'css', 'jQuery'),
+        'link' => array(
+          'url' => 'http://broadwayjoe.tv',
+          'text' => 'Visit Website'
+        )
+      ),
+      array(
+        'name' => 'Facebook Welcome Page',
+        'image' => '/images/portfolio/bj-3.jpg',
+        'desc' => 'With a growing fan-base on Facebook, Joe wanted to have a default welcome tab that would let people know what Broadway Joe was all about.  I coded and designed this tab for that purpose.',
+        'skills' => array('photoshop', 'php', 'fbml', 'html', 'css'),
+        'link' => array(
+          'url' => 'http://www.facebook.com/JoeNamath?v=app_4949752878',
+          'text' => 'facebook tab'
+        )
+      ),
+      array(
+        'name' => 'Twitter Background',
+        'image' => '/images/portfolio/bj-4.jpg',
+        'desc' => 'We wanted to keep the Twitter background visible to the most eyes so it was designed for screen resolutions 1280px in width on up. As I designed Joe\'s background, I wanted to highlight the website url, other social media sites, and overall BroadwayJoe.tv branding.',
+        'skills' => array('photoshop'),
+        'link' => array(
+          'url' => 'http://twitter.com/realjoenamath',
+          'text' => 'twitter page'
+        )
+      ),
+      array(
+        'name' => 'YouTube Background',
+        'image' => '/images/portfolio/bj-5.jpg',
+        'desc' => 'Joe wanted to create a YouTube channel to house the videos from his blog.  My task was to brand it BroadwayJoe.tv',
+        'skills' => array('photoshop'),
+        'link' => array(
+          'url' => 'http://www.youtube.com/user/BroadwayJoeTV',
+          'text' => 'youtube channel'
+        )
+      )
+    )
+  );
+  
+  $kerry = array(
+    'name' => 'KerryRhodes.com',
+    'subtitle' => 'Kerry Rhodes\' Personal Website',
+    'desc' => 'As a huge football fan, it was a great experience to design and code this project.  Joe wanted to create a website where he essentially housed his video blog and at the same time act as his personal social media hub.  This project required a complete product from logo creation to social media branding.',
+    'projects' => array(
+      array(
+        'name' => 'Website',
+        'image' => '/images/portfolio/kr-2.jpg',
+        'desc' => 'A fun site to work on both on the design and social media side of things.  One of the first projects where I went coast to coast on an entire project from ideation to final css tweaks.',
+        'skills' => array('photoshop', 'wordpress', 'merchdirect', 'php', 'html', 'css', 'jQuery'),
+        'link' => array(
+          'url' => '/preview.php?image=/images/preview/kr-2.jpg',
+          'text' => 'sample image'
+        )
+      ),
+      array(
+        'name' => 'Facebook Welcome Page',
+        'image' => '/images/portfolio/kr-3.jpg',
+        'desc' => 'With a huge presence even outside of playing in the NFL, Kerry wanted to have a welcome tab that would let people know about all the other social media platforms he was on.  This is the coded and designed creation that I made for him.',
+        'skills' => array('photoshop', 'php', 'fbml', 'html', 'css'),
+        'link' => array(
+          'url' => '/preview.php?image=/images/preview/kr-3.jpg',
+          'text' => 'sample image'
+        )
+      ),
+      array(
+        'name' => 'Twitter Background',
+        'image' => '/images/portfolio/kr-4.jpg',
+        'desc' => 'Before we even started working with Kerry, he had a huge following on Twitter.  We wanted to keep the Twitter background visible to the most eyes so it was designed for screen resolutions 1280px in width on up. I wanted to highlight the website url, other social media sites, and overall KerryRhodes.com branding.',
+        'skills' => array('photoshop'),
+        'link' => array(
+          'url' => '/preview.php?image=/images/preview/kr-4.jpg',
+          'text' => 'sample image'
+        )
+      ),
+      array(
+        'name' => 'YouTube Background',
+        'image' => '/images/portfolio/kr-5.jpg',
+        'desc' => 'I was in charge of designing the background for Kerry\'s YouTube background.  This is where he would house all the videos from his website that were dedicated to his vlog.',
+        'skills' => array('photoshop'),
+        'link' => array(
+          'url' => '/preview.php?image=/images/preview/kr-5.jpg',
+          'text' => 'sample image'
+        )
+      ),
+      array(
+        'name' => 'UStream Background',
+        'image' => '/images/portfolio/kr-6.jpg',
+        'desc' => 'As an avid UStreamer, Kerry wanted to brand his UStream channel similar to that of his website.  I designed his UStream background with that in mind and wanted to show case the vast array of other social media platforms he was on.',
+        'skills' => array('photoshop'),
+        'link' => array(
+          'url' => '/preview.php?image=/images/preview/kr-6.jpg',
+          'text' => 'sample image'
+        )
+      ),
+      array(
+        'name' => 'Arizona Cardinals Re-Design',
+        'image' => '/images/portfolio/kr-1.jpg',
+        'desc' => 'So when Kerry got traded to the Cardinals, we wanted to quickly re-brand his website with a new color scheme and updated images.  It was my job on the front-end and design side to make this happen.',
+        'skills' => array('photoshop','html', 'css'),
+        'link' => array(
+          'url' => 'http://kerryrhodes.com',
+          'text' => 'visit website'
+        )
+      )
+    )
+  );
+  
+  $jalen = array(
+    'name' => 'JalenRose.com',
+    'subtitle' => 'Jalen Rose\'s Personal Website',
+    'desc' => 'As a huge UNC fan, I will never forget the Fab Five and the C. Web Timeout.  None the less I am also a huge Michigan Football fan, so when I had the opportunity to work on Jalen Rose\'s website I was really excited.  Jalen had a lot to say and was and still is involved in a lot of various organizations (ESPN, Rose Foundation, etc...).',
+    'projects' => array(
+      array(
+        'name' => 'Logo / Branding',
+        'image' => '/images/portfolio/jr-1.jpg',
+        'desc' => 'I really had fun creating a crest for Jalen\'s branding.  It was a real chance to be creative and actually brand a well known celebrity.  Later it was even used for store items.',
+        'skills' => array('photoshop','illustrator'),
+        'link' => array(
+          'url' => '/preview.php?image=/images/preview/jr-1.jpg',
+          'text' => 'sample image'
+        )
+      ),
+      array(
+        'name' => 'Website',
+        'image' => '/images/portfolio/jr-2.jpg',
+        'desc' => 'As it was my first time ever doing a personal website, I was really excited. It was my task to layout and design a website that would narrate Jalen\'s past, present, and future while integrating several social media platforms.',
+        'skills' => array('photoshop', 'wordpress', 'php', 'html', 'css'),
+        'link' => array(
+          'url' => 'http://jalenrose.com',
+          'text' => 'visit website'
+        )
+      ),
+      array(
+        'name' => 'Facebook Welcome Page',
+        'image' => '/images/portfolio/jr-3.jpg',
+        'desc' => 'It was my job to create a default tab for fans to land on when they visited Jalen\'s Facebook Fan Page.  We wanted to have a welcome video and highlight all his other social media links.',
+        'skills' => array('photoshop', 'php', 'fbml', 'html', 'css'),
+        'link' => array(
+          'url' => '/preview.php?image=/images/preview/jr-3.jpg',
+          'text' => 'sample image'
+        )
+      ),
+      array(
+        'name' => 'Twitter Background',
+        'image' => '/images/portfolio/jr-4.jpg',
+        'desc' => 'I designed Jalen\'s Twitter background to accommodate screen resolutions 1280px wide and up, while matching the style of his website and promote the other social media platforms he was on.',
+        'skills' => array('photoshop'),
+        'link' => array(
+          'url' => 'http://twitter.com/jalenrose',
+          'text' => 'twitter page'
+        )
+      ),
+      array(
+        'name' => 'YouTube Background',
+        'image' => '/images/portfolio/jr-5.jpg',
+        'desc' => 'Jalen wanted to brand his YouTube channel as JalenTV yet still bring in the branding and design elements from his site.',
+        'skills' => array('photoshop'),
+        'link' => array(
+          'url' => 'http://www.youtube.com/user/jalentv',
+          'text' => 'youtube channel'
+        )
+      ),
+      array(
+        'name' => 'UStream Background',
+        'image' => '/images/portfolio/jr-6.jpg',
+        'desc' => 'When I designed Jalen\'s UStream page I wanted to bring in the crest element from his website branding to give his channel a unique look.',
+        'skills' => array('photoshop', 'illustrator'),
+        'link' => array(
+          'url' => 'http://www.ustream.tv/channel/therosereport',
+          'text' => 'ustream channel'
+        )
+      )
+    )
+  );
+  
+  $hhal = array(
+    'name' => 'HipHopAtLunch.com',
+    'subtitle' => 'Hip Hop Blog',
+    'desc' => 'I am from the south so working on a hip hop blog was surprisingly fun.  Working with Mike Boyd Jr. was a pleasure and he knew exactly what he wanted from a functionality standpoint, however on the design side, all he told me was, "I want it to be ice cream paint job… you know… cream on the inside, clean on the outside" (A reference to Dorrough Music - Ice Cream Paint Job).',
+    'projects' => array(
+      array(
+        'name' => 'Logo / Branding',
+        'image' => '/images/portfolio/hhal-1.jpg',
+        'desc' => 'Boyd wanted to keep the logo simple, clean, and easy to read.  It was my task to create a logo that was legible yet easily associated with HipHopAtLunch.com and establish a brand.',
+        'skills' => array('photoshop','illustrator'),
+        'link' => array(
+          'url' => '/preview.php?image=/images/preview/hhal-1.jpg',
+          'text' => 'sample image'
+        )
+      ),
+      array(
+        'name' => 'Website',
+        'image' => '/images/portfolio/hhal-2.jpg',
+        'desc' => 'My whole inspiration for Hip Hop At Lunch was that Boyd wrote about up and coming artists, mainly mixtapes.  I wanted to keep the design clean and simple and make everything around it resemble an old school tape recorder.  I was responsible for all the development and design.',
+        'skills' => array('photoshop', 'wordpress', 'php', 'html', 'css'),
+        'link' => array(
+          'url' => 'http://hiphopatlunch.com',
+          'text' => 'visit Website'
+        )
+      ),
+      array(
+        'name' => 'Twitter Background',
+        'image' => '/images/portfolio/hhal-3.jpg',
+        'desc' => 'I wanted to optimize the background for 1280px width screen resolutions while providing branding and social media context for Hip Hop At Lunch.',
+        'skills' => array('photoshop'),
+        'link' => array(
+          'url' => 'http://twitter.com/hiphopatlunch',
+          'text' => 'twitter page'
+        )
+      ),
+      array(
+        'name' => 'Artist Section',
+        'image' => '/images/portfolio/hhal-4.jpg',
+        'desc' => 'For the longest time the Artists button on Hip Hop At Lunch was coming soon, but when we finally got a chance to work on it Boyd and I wanted to create a wikipedia of sorts for artists that weren\'t as well known or popular enough to have their own wikipedia.  All of this with a twist - a social media twist.',
+        'skills' => array('photoshop', 'wordpress', 'facebook widgets', 'twitter widgets', 'php', 'html', 'css'),
+        'link' => array(
+          'url' => 'http://hiphopatlunch.com/artists/',
+          'text' => 'visit website'
+        )
+      )
+    )
+  );
+
+
+  $clients = array(
+    'bjtv' => $bjtv,
+    'nets' => $nets,
+    'hhal' => $hhal,
+    'kerry' => $kerry,
+    'jalen' => $jalen,
+    'jets' => $jets
+  );
+?>
