b1a511da4af8bf9c236ebb774eeec47acee0cc4d
Sameer <Sameer@Sameers-MacBook-Pro-4.local>
2010-11-04 13:06:00 -0400

    temp portfolio - analytics

--- index.php
+++ index.php
@@ -40,6 +40,20 @@
   </div> <!-- end of #container -->
 
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+  <script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-19511860-1']);
+    _gaq.push(['_setDomainName', '.sameert.com']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+  </script>
   <script>!window.jQuery && document.write('<script src="js/jquery-1.4.2.min.js"><\/script>')</script>
   <script src="js/plugins.js?v=1"></script>
   <script src="js/script.js?v=1"></script>
